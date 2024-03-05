db34cf06501016058d53491044bc49f946e42e97
NeuralNuts <wilsonzakk8@gmail.com>
2024-03-05 13:36:54 +1000

    Trying to turn on the buzzer

--- src/main.c
+++ src/main.c
@@ -1,9 +1,20 @@
 #include "../include/attiny1626_display.h"
 
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
 int main() {
     struct BinaryAlphabet binary_alphabet;
 
+    uint8_t port5 = 0b00100000;
+
+    PORTB.DIRSET = PIN0_bm;
+
     for(;;){
-        display_message("g", _1000_MILLISECOND_DELAY, binary_alphabet);
+        PORTB.OUTTGL = PIN0_bm;
+
+        _delay_ms(1000);
+        //display_message("g", _1000_MILLISECOND_DELAY, binary_alphabet);
     }
 }
