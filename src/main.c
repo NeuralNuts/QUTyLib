87a948f91d85412d823572411a198d820ec9289a
NeuralNuts <wilsonzakk8@gmail.com>
2024-03-06 21:59:03 +1000

    sandstorm

--- src/main.c
+++ src/main.c
@@ -9,16 +9,18 @@
 #include <unistd.h>
 
 void tca_init(uint16_t freq) {
+    PORTMUX.TCAROUTEA = PORTMUX_TCA00_bm;
+
     PORTB.DIRSET = PIN0_bm;
 
-    // Set waveform generation mode to single slope
+    //  WGM set to single slope
     TCA0.SINGLE.CTRLB = TCA_SINGLE_WGMODE_SINGLESLOPE_gc | TCA_SINGLE_CMP1EN_bm;
     //TCA0.SINGLE.CTRLB = TCA_SINGLE_WGMODE_DSBOTTOM_gc | TCA_SINGLE_CMP1EN_bm;
 
-    // Set period to control PWM frequency
+    // period to control PWM frequency
     TCA0.SINGLE.PER = freq;
 
-    // Set duty cycle to 50% initially
+    // Set duty cycle initially
     TCA0.SINGLE.CMP1 = 1000;
 
     // Enable TCA0 overflow interrupt
@@ -29,33 +31,391 @@ void tca_init(uint16_t freq) {
 }
 
 ISR(TCA0_OVF_vect) {
-    // Your ISR code here
-
     PORTB.OUTTGL = PIN0_bm;
 
     TCA0.SINGLE.INTFLAGS = TCA_SINGLE_OVF_bm;
 }
 
+#define FREQ_BASE 90000 * 9 
+#define FREQ_BASE_2 50000 * 2
+#define DELAY_BASE 30
+#define DELAY_BASE_HALF 60 / 4
+
+void note(uint16_t freq, uint16_t duration) {
+    PORTB.OUTCLR = PIN0_bm; // Toggle the output pin connected to the buzzer
+
+    tca_init(freq); // Initialize TCA for the given frequency
+
+    for (uint16_t i = 0; i < duration; i++) {
+        _delay_ms(1); // Delay for 1 millisecond
+    }
+
+
+    // Turn off the note
+    TCA0.SINGLE.CTRLA = 0; // Disable TCA
+    PORTB.OUTSET = PIN0_bm; // Set the pin high
+}
+
+// Play the melody
+void compose() {
+     // Si Si Si Si Si
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+
+    // Re Si Si Si Si Si
+    note(FREQ_BASE / 233, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+
+    // Si Si Si Si Si
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+
+    // Si Si Si Si Si
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+
+    // Si Si Si Si Si
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+
+    // Si Si Si Si Si Si Si Si Si Si Si
+    note(FREQ_BASE / 247, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+
+    // Mi Mi Mi Mi Mi Mi Mi
+    note(FREQ_BASE / 330, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 330, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 330, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 330, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 330, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 330, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 330, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+
+    // Re Re Re Re Re Re Re
+    note(FREQ_BASE / 294, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 294, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 294, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 294, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 294, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 294, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 294, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+
+    // La La Si Si Si Si Si Si Si Si Si Si
+    note(FREQ_BASE / 440, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 440, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+
+    // Mi Mi Si Si Si Si Si Si Si Si Si Si
+    note(FREQ_BASE / 330, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 330, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+
+    // Mi Mi
+    note(FREQ_BASE / 330, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 330, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+
+    // Si Si Si Si Si Si Si Si Si Si Si
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+
+    // Mi Mi Mi Mi Mi Mi Mi
+    note(FREQ_BASE / 330, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 330, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 330, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 330, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 330, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 330, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 330, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+
+    // Re Re Re Re Re Re Re
+    note(FREQ_BASE / 294, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 294, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 294, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 294, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 294, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 294, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 294, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+
+    // La Si Si Si Si Si Si Si Si Si Si Si Si
+    note(FREQ_BASE / 440, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+
+    // Mi Mi Si Si Si Si Si Si Si Si Si Si
+    note(FREQ_BASE / 330, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 330, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 494, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+
+    // Mi Mi
+    note(FREQ_BASE / 330, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 330, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+
+    // Si Si Si Si Si Si Si Si Si Si Si
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 247, DELAY_BASE * 2);
+    _delay_ms(DELAY_BASE);
+
+    // Mi Mi Mi Mi Mi Mi Mi
+    note(FREQ_BASE / 330, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 330, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 330, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 330, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 330, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 330, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 330, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+
+    // Re Re Re Re Re Re Re
+    note(FREQ_BASE / 294, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 294, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 294, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 294, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 294, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 294, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+    note(FREQ_BASE / 294, DELAY_BASE * 4);
+    _delay_ms(DELAY_BASE);
+}
+
 int main() {
     cli();
-
-    //tca_init(9000);
-
     sei();
 
     struct BinaryAlphabet ba;
 
-    // Main program loop
     while(1) {
-        //display_message("hello ", _250_MILLISECOND_DELAY, ba);
-
-        PORTB.OUTCLR ^= PIN0_bm;
-        _delay_ms(50);
-        PORTB.OUTCLR ^= PIN0_bm;
-        _delay_ms(70);
-        PORTB.OUTCLR &= PIN0_bm;
-
-        //display_message("", _500_MILLISECOND_DELAY, ba);
+        compose();
     }
+
     return 0;
 }
