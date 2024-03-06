76b45aceed94289c5d72178b5e3857de42bf5e94
NeuralNuts <wilsonzakk8@gmail.com>
2024-03-06 16:52:55 +1000

    bad beats

--- src/main.c
+++ src/main.c
@@ -8,7 +8,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
-void tca_init() {
+void tca_init(uint16_t freq) {
     PORTB.DIRSET = PIN0_bm;
 
     // Set waveform generation mode to single slope
@@ -16,10 +16,10 @@ void tca_init() {
     //TCA0.SINGLE.CTRLB = TCA_SINGLE_WGMODE_DSBOTTOM_gc | TCA_SINGLE_CMP1EN_bm;
 
     // Set period to control PWM frequency
-    TCA0.SINGLE.PER = 1000;
+    TCA0.SINGLE.PER = freq;
 
     // Set duty cycle to 50% initially
-    TCA0.SINGLE.CMP1 = 255;
+    TCA0.SINGLE.CMP1 = 1000;
 
     // Enable TCA0 overflow interrupt
     TCA0.SINGLE.INTCTRL = TCA_SINGLE_OVF_bm;
@@ -38,11 +38,24 @@ ISR(TCA0_OVF_vect) {
 
 int main() {
     cli();
-    tca_init();
+
+    //tca_init(9000);
+
     sei();
+
+    struct BinaryAlphabet ba;
 
     // Main program loop
     while(1) {
+        //display_message("hello ", _250_MILLISECOND_DELAY, ba);
+
+        PORTB.OUTCLR ^= PIN0_bm;
+        _delay_ms(50);
+        PORTB.OUTCLR ^= PIN0_bm;
+        _delay_ms(70);
+        PORTB.OUTCLR &= PIN0_bm;
+
+        //display_message("", _500_MILLISECOND_DELAY, ba);
     }
     return 0;
 }
